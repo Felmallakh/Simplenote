1e1c6dd5f1ac15472192641e3b3274ca4fdf6ed6
felmallakh <fady_elmallakh@hotmail.com>
2021-12-04 13:09:31 -0800

    phase 2 done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import LoginForm from "./components/LoginForm";
 import * as sessionActions from "./store/session";
+import LoginForm from "./components/LoginForm";
+import SignupForm from "./components/SignupForm";
 
 function App() {
   const dispatch = useDispatch();
@@ -17,6 +18,9 @@ function App() {
         <h1>Hello from App</h1>
         <Route path="/login">
           <LoginForm />
+        </Route>
+        <Route path="/signup">
+          <SignupForm />
         </Route>
       </Switch>
     )
--- frontend/src/components/LoginForm/index.js
+++ frontend/src/components/LoginForm/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { loginUser } from "../../store/session";
+import * as sessionActions from "../../store/session";
 import "./LoginForm.css";
 
 
@@ -17,7 +17,7 @@ function LoginForm() {
   const handleSubmit = (e) => {
     e.preventDefault();
     setErrors([]);
-    return dispatch(loginUser({ credential, password })).catch(
+    return dispatch(sessionActions.loginUser({ credential, password })).catch(
       async (res) => {
         const data = await res.json();
         if (data && data.errors) setErrors(data.errors);
@@ -26,32 +26,35 @@ function LoginForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => (
-          <li key={idx}>{error}</li>
-        ))}
-      </ul>
-      <label>
-        Username or Email
-        <input
-          type="text"
-          value={credential}
-          onChange={(e) => setCredential(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Log In</button>
-    </form>
+    <div className="form-container">
+      <form className="user-form" onSubmit={handleSubmit}>
+        <h1>Login form</h1>
+        <ul className="user-form-errors">
+          {errors.map((error, idx) => (
+            <li key={idx}>{error}</li>
+          ))}
+        </ul>
+        <label>
+          Username or Email
+          <input
+            type="text"
+            value={credential}
+            onChange={(e) => setCredential(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <button type="submit">Log In</button>
+      </form>
+    </div>
   );
 }
 
