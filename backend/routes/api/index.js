bcc67c6db8e35a462d29beb896f3b3b2255e07c9
felmallakh <fady_elmallakh@hotmail.com>
2021-12-04 00:53:17 -0800

    tested middlewares in api-index.js

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,16 +1,27 @@
 const router = require('express').Router();
 const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth');
 
+const { setTokenCookie, restoreUser, requireAuth } = require("../../utils/auth");
 const { User } = require('../../db/models');
 
-router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
-    const user = await User.findOne({
-        where: { username: 'Demo'}
-    });
-    setTokenCookie(res, user);
-    return res.json({ user });
-}))
+// testing token cookie ... check cookie created in DevTools
+// router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+//     const user = await User.findOne({
+//         where: { username: 'Demo'}
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+// }))
+
+// // testing restoreUser middleware
+// router.get("/restore-user", restoreUser, (req, res) => {
+//   return res.json(req.user);
+// });
+
+// // testing requireAuth middleware
+// router.get('/require-auth', requireAuth, (req, res) => {
+//     return res.json(req.user);
+// });
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
