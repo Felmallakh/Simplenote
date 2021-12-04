2ca31fbb13e030178905d8dfb248b61e33b49cda
felmallakh <fady_elmallakh@hotmail.com>
2021-12-04 09:00:07 -0800

    csrf & index store files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,13 +12,11 @@ import * as sessionActions from './store/session';
 
 const store = configureStore();
 
-// attach the custom csrfFetch function
 if (process.env.NODE_ENV !== "production") {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
   window.store = store;
-  window.sessionActions = sessionActions;
 }
 
 function Root() {
